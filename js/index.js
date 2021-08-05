c53ec5614d3d7f5c2a2b6477a8944c7497ce246a
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-08-05 13:04:58 -0400

    add loadedmetadata

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 const video = document.querySelector('#video')
 const play = document.querySelector('#play')
 const pause = document.querySelector('#pause')
+const backward = document.querySelector('#backward')
+const forward = document.querySelector('#forward')
+const progress = document.querySelector('#range')
+
 
 const handlePlay = () => {
     video.play()
@@ -16,5 +20,29 @@ const handlePause = () => {
     console.log('pause');
 }
 
+const handleBackward = () => {
+    video.currentTime -= 10
+    console.log('atras 10 seg');
+}
+
+const handleForward = () => {
+    video.currentTime += 10
+    console.log('adelante 10 seg');
+}
+
+const handleLoaded = () => {
+    progress.max = video.duration
+    console.log('video cargado', video.duration);
+}
+
+const handleTimeUpdate = () => {
+    progress.value = video.currentTime
+    console.log('tiempo actual', video.currentTime);
+}
+
 play.addEventListener('click', handlePlay)
 pause.addEventListener('click', handlePause)
+backward.addEventListener('click', handleBackward)
+forward.addEventListener('click', handleForward)
+video.addEventListener('loadedmetadata', handleLoaded)
+video.addEventListener('timeupdate', handleTimeUpdate)
