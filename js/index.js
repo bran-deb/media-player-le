465c311f417a4011d35bc301b3b93b2ef2833a24
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-08-04 23:45:43 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const video = document.querySelector('#video')
+const play = document.querySelector('#play')
+const pause = document.querySelector('#pause')
+
+play.addEventListener('click', handlePlay)
+pause.addEventListener('click', handlePause)
+
+function handlePlay() {
+    video.play()
+    play.hidden = true
+    pause.hidden = false
+    console.log('play');
+}
+
+function handlePause() {
+    video.pause()
+    play.hidden = false
+    pause.hidden = true
+    console.log('pause');
+}
